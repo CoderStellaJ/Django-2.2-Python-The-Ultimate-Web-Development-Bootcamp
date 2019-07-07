e509bbe41b917c3771bd5a7d4a57a8c3daae2dcb
CoderStellaJ <40347063+CoderStellaJ@users.noreply.github.com>
2019-07-07 10:43:23 +0800

    update job part

--- Site2 Portfolio/portfolio-project/blog/models.py
+++ Site2 Portfolio/portfolio-project/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+# Create A blog models
+# title
+# pub_date
+# body
+# image
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+# Add the Blog app to settings
+
+# Create a migration
+
+# Migrate
+
+# Add to the admin
